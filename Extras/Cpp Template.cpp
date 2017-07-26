14b3e5a2f4057340bd23d904ce23c460a29e1ad8
saraansh <bsaraansh@gmail.com>
2017-07-27 03:56:35 +0530

    Updated C++ Template

--- Extras/Cpp Template.cpp
+++ Extras/Cpp Template.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 #define fill(arr,val) memset(arr,val,sizeof(arr))
-#define ll long long;
+#define ll long long
 #define forn(i,n) for(int i=0; i<int(n); i++)
-#define endl "\n";
-#define inf 0x3f3f3f3f;
-#define inf1 ll(2e18)+ll(1e10);
+#define endl "\n"
+#define inf 0x3f3f3f3f
+#define inf1 ll(2e18)+ll(1e10)
 
 int main(){
 	ios_base::sync_with_stdio(false);
