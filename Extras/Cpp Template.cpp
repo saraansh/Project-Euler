e70f63e5ec4bfa3bf41eb893581ab9ceda9b6bf9
saraansh <bsaraansh@gmail.com>
2017-07-27 03:10:02 +0530

    C++ Template

---
+++ Extras/Cpp Template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fill(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long;
+#define forn(i,n) for(int i=0; i<int(n); i++)
+#define endl "\n";
+#define inf 0x3f3f3f3f;
+#define inf1 ll(2e18)+ll(1e10);
+
+int main(){
+
+
+
+	return 0;
+}
